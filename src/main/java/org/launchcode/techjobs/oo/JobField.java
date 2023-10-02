949bf7e939b79d7383bc744f5ea0d8f159ef5989
anamindful <anamindful@gmail.com>
2023-10-02 17:11:10 -0500

    equals added to JobField

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -18,6 +18,12 @@ public abstract class JobField {
     }
 
     //toString, equals, hashCode methods
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
 
     @Override
     public String toString() {
